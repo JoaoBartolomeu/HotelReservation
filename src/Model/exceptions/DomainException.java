5c506c58d046f3ccce40fdce220052b1816fd6f2
JoaoBartolomeu <joaosnpr@gmail.com>
2024-11-01 01:07:54 -0300

    Alterado o extends para RuntimeException - Não me obriga a tratar as exceções quando a classe for utilizada

--- src/Model/exceptions/DomainException.java
+++ src/Model/exceptions/DomainException.java
@@ -1,6 +1,6 @@
 package Model.exceptions;
 
-public class DomainException extends Exception{
+public class DomainException extends RuntimeException{
     public static final long serialVersionUID = 1L;
     public DomainException(String msg) {
         super(msg);
