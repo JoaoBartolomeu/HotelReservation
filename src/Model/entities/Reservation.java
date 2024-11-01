f5578f9a533afddae1704ed3c483513573e5c6ac
JoaoBartolomeu <joaosnpr@gmail.com>
2024-11-01 00:02:15 -0300

    Aplicado as lógicas das funções e sobrescrita do método toString

--- src/Model/entities/Reservation.java
+++ src/Model/entities/Reservation.java
@@ -1,4 +1,70 @@
 package Model.entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Reservation {
+    private Integer roomNumber;
+    private Date checkinDate;
+    private Date checkoutDate;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(){}
+    public Reservation(Date checkoutDate, Date checkinDate, Integer roomNumber) {
+        this.checkoutDate = checkoutDate;
+        this.checkinDate = checkinDate;
+        this.roomNumber = roomNumber;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckinDate() {
+        return checkinDate;
+    }
+
+    public void setCheckinDate(Date checkinDate) {
+        this.checkinDate = checkinDate;
+    }
+
+    public Date getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(Date checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
+    public Integer duration(){
+        if (checkinDate == null || checkoutDate == null){
+            throw new IllegalArgumentException("As datas de check-in e check-out não podem ser nulas.");
+        }
+        if (checkoutDate.before(checkinDate)){
+            throw new IllegalArgumentException("A data de check-out não pode ser anterior à data de check-in.");
+        }
+        long diffInMillies = checkoutDate.getTime() - checkinDate.getTime();
+        return (int) TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkinDate, Date checkoutDate){
+        this.checkoutDate = checkoutDate;
+        this.checkinDate = checkinDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation: " +
+                "Room " + roomNumber +
+                ", check-in: " + sdf.format(checkinDate) +
+                ", check-out: " + sdf.format(checkoutDate) + " "
+                + duration()  + " nights";
+    }
 }
+
